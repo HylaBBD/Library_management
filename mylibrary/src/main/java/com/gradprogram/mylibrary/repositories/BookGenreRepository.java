d0fad80c433e8782c47d1e103fa05302bcaca8e5
Thomas Stevens <thomas@bbd.co.za>
2023-03-17 13:21:20 +0200

    Forgot to fix genre's. Fixed now

--- mylibrary/src/main/java/com/gradprogram/mylibrary/repositories/BookGenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gradprogram.mylibrary.repositories;
-
-import com.gradprogram.mylibrary.models.BookGenres;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookGenreRepository extends JpaRepository<BookGenres, Long> {
-}
